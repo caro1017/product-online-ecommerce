736d9f32445ac6938d17797bb0c18ea4692dcdc9
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2025-09-11 23:13:30 -0500

    Ajuste en el enrutamiento login

--- src/components/pages/RegisteredPage/Section/Login.jsx
+++ src/components/pages/RegisteredPage/Section/Login.jsx
@@ -37,7 +37,7 @@ export const Login = () => {
         severity: "success",
         variant: "filled",
       });
-      setTimeout(() => navigate("/"), 2000);
+      setTimeout(() => navigate("/home"), 2000);
     } catch (err) {
       console.error("Error en login:", {
         message: err.message,
